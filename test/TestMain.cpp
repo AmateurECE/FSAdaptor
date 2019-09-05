85e51f03b9dd268befc5de59a7147715858f73d3
AmateurECE <edtwardy@mtu.edu>
2019-09-05 11:58:41 -0400

    Implement walk() method
    This path implements the walk() method in the Standard Filesystem
    Adaptor. Integration tests have not yet been developed for this
    method, but some simple confidence testing was done to verify basic
    functionality.

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -7,8 +7,13 @@
 //
 // CREATED:         08/18/2019
 //
-// LAST EDITED:     09/03/2019
+// LAST EDITED:     09/05/2019
 ////
+
+// TODO: Update and refactor unit tests
+// The goal here is to have a single test fixture for each behavior of the Path
+// interface, and each test fixture is responsible only for testing a single
+// behavior.
 
 #include <gtest/gtest.h>
 
