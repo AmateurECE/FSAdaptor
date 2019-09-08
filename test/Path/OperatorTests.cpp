5f89b2e86ab15a87d77b926c3eb9d2c2a72f545b
AmateurECE <edtwardy@mtu.edu>
2019-09-08 18:51:29 -0400

    Implement new operators for Path
    This patch implements new operator functions for the Path class. In
    addition, refactor StandardFilesystemAdaptor::createDirectories() to
    use the new operators. Finally, add tests for the new operators. Also
    fix a bug in the Iterator.

---
+++ test/Path/OperatorTests.cpp
@@ -0,0 +1,68 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            OperatorTests.cpp
+//
+// AUTHOR:          Ethan D. Twardy <edtwardy@mtu.edu>
+//
+// DESCRIPTION:     Tests for some of the operators.
+//
+// CREATED:         09/08/2019
+//
+// LAST EDITED:     09/08/2019
+////
+
+#include "gtest/gtest.h"
+
+#include <FSAdaptor/Path.h>
+
+#include <string>
+
+class OperatorTests : public ::testing::Test
+{};
+
+TEST_F(OperatorTests, PlusEqualsOne)
+{
+  FSAdaptor::Path pOne{"/a/"};
+  FSAdaptor::Path pTwo{"dir"};
+  pOne += pTwo;
+  ASSERT_EQ(pOne.string(), "/a/dir");
+}
+
+TEST_F(OperatorTests, PlusEqualsTwo)
+{
+  FSAdaptor::Path pOne{"/a"};
+  FSAdaptor::Path pTwo{"dir"};
+  pOne += pTwo;
+  ASSERT_EQ(pOne.string(), "/adir");
+}
+
+TEST_F(OperatorTests, DivEqualsOne)
+{
+  FSAdaptor::Path pOne{"/a/"};
+  FSAdaptor::Path pTwo{"dir"};
+  pOne /= pTwo;
+  ASSERT_EQ(pOne.string(), "/a/dir");
+}
+
+TEST_F(OperatorTests, DivEqualsTwo)
+{
+  FSAdaptor::Path pOne{"/a"};
+  FSAdaptor::Path pTwo{"dir"};
+  pOne /= pTwo;
+  ASSERT_EQ(pOne.string(), "/a/dir");
+}
+
+TEST_F(OperatorTests, IsEqual)
+{
+  FSAdaptor::Path pOne{"a"};
+  FSAdaptor::Path pTwo{"a"};
+  ASSERT_TRUE(pOne == pTwo);
+}
+
+TEST_F(OperatorTests, IsNotEqual)
+{
+  FSAdaptor::Path pOne{"a"};
+  FSAdaptor::Path pTwo{"b"};
+  ASSERT_TRUE(pOne != pTwo);
+}
+
+///////////////////////////////////////////////////////////////////////////////
